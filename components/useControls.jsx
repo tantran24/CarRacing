ca8ae7240a2f3dfb35832ccf31f084301d4c32ea
DaoNamThuan <21522649@gm.uit.edu.vn>
2024-06-09 14:30:13 +0700

    commit changes in local to pull

--- components/useControls.jsx
+++ components/useControls.jsx
@@ -37,11 +37,11 @@ export const useControls = (vehicleApi, chassisApi) => {
             console.log("forward");
             // vehicleApi.applyEngineForce(150, 2);
             // vehicleApi.applyEngineForce(150, 3);
-            vehicleApi.applyEngineForce(1000, 2);
-            vehicleApi.applyEngineForce(1000, 3);
+            vehicleApi.applyEngineForce(600, 2);
+            vehicleApi.applyEngineForce(600, 3);
         } else if (controls.s) {
-            vehicleApi.applyEngineForce(-600, 2);
-            vehicleApi.applyEngineForce(-600, 3);
+            vehicleApi.applyEngineForce(-300, 2);
+            vehicleApi.applyEngineForce(-300, 3);
         } else {
             vehicleApi.applyEngineForce(0, 2);
             vehicleApi.applyEngineForce(0, 3);
